292de3aea6b0575671083765cf210e51b1be4ecb
Warrier-karthik <karthikwarrier226@gmail.com>
2025-02-25 19:39:56 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   routes/index.js 	modified:   routes/user.js 	modified:   server.js 	modified:   views/index.ejs 	new file:   views/layout.ejs 	new file:   views/layouts/message.ejs 	modified:   views/user.ejs

--- routes/user.js
+++ routes/user.js
@@ -2,35 +2,55 @@ const express = require('express');
 const router = express.Router()
 const db = require('../database')
 const bcrypt = require('bcrypt')
+let msg;
 //Get URL
-router.get('/', (req, res) => {
-    res.render('user')
+router.get('/', async (req, res) => {
+    const user = await db.getuser(req.session.name)
+    res.render('user', {user:user[0]})
 })
 
 router.get('/register', (req, res) => {
-    res.render('register')
+    res.render('register', {msg:msg})
 })
 router.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login', {msg: msg})
 })
 
 //POST URL
 router.post('/register', async (req, res) => {
+    const user = await db.getuser(req.body.username)
     const encryptedpwd = await bcrypt.hash(req.body.password, 10)
-    const result = await db.createUser(req.body.username, req.body.Fname, req.body.Lname, req.body.email, encryptedpwd)
-    res.redirect('./login')
+    if (user.length > 0 ){
+        res.redirect('./register')
+        msg = "username already exists"
+    }
+    
+    else if (req.body.password != req.body.Cpassword){
+            res.redirect('./register')
+            msg = "User already exist."
+        }
+    else {
+        const result = await db.createUser(req.body.username, req.body.Fname, req.body.Lname, req.body.email, encryptedpwd)
+        res.redirect('./login')
+        msg = ""
+    }
+    
 })
+
 router.post('/login', async (req, res) => {
     const user = await db.getuser(req.body.username)
     if (await bcrypt.compare(req.body.password, user[0].Password))
     {
-        console.log("Welcome to my site");
+        req.session.isAuthenticated = true;
+        req.session.name = req.body.username;
+        msg = "";
         res.redirect('/')
     }
     else
     {
-        console.log("Wtf wrong fing password bruh....")
+        msg = "Credentials do not match"
         res.redirect('./login')
+        
     }
 })
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require("express")
 const app = express()
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/user')
+const session = require('express-session')
+const layout = require('express-ejs-layouts')
+app.use(session({
+    secret: "messi@10",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isAuthenticated;
+    res.locals.name = req.session.name
+    next();
+})
+app.use(layout)
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views')
 
